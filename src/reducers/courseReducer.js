51199c1115267bbafc690dfc42e924fdf4dec1f7
James Kelly <jk_jameskelly@outlook.com>
2017-03-17 16:46:18 -0400

    use mapDispatchToProps

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 export default function courseReducer(state = [], action) {
   switch(action.type) {
     case 'CREATE_COURSE':
+      //debugger;
       return [...state, //spread operator
         Object.assign({}, action.course)
       ];
