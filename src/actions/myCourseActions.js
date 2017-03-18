5ae5e0af6f981a20e85c3592571f5c77c26fb125
James Kelly <jk_jameskelly@outlook.com>
2017-03-18 10:44:14 -0400

    use thunk to load data from mock api

--- src/actions/myCourseActions.js
+++ src/actions/myCourseActions.js
@@ -1,6 +1,21 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
+export function loadCoursesSuccess(courses) {
   //debugger;
-  return { type: types.CREATE_COURSE, course: course };
+  return { type: types.LOAD_COURSES_SUCCESS, courses: courses };
+}
+
+
+
+export function loadCourses() {
+  //thunk always returns function that accepts a dispatch
+
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import appRoutes from './routes';
+import {loadCourses} from './actions/myCourseActions';
 import './styles/styles.css'; //webpack can import css files
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
+
 
 render(
   <Provider store={store}>
