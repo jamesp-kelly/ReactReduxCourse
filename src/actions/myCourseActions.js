e2bf68ed06e3ab00f984af6ce9fc9faf5069e1c1
James Kelly <jk_jameskelly@outlook.com>
2017-03-22 14:26:50 -0400

    add course

--- src/actions/myCourseActions.js
+++ src/actions/myCourseActions.js
@@ -6,6 +6,14 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses: courses };
 }
 
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course};
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course}; 
+}
+
 
 
 export function loadCourses() {
@@ -18,4 +26,15 @@ export function loadCourses() {
       throw(error);
     });
   };
+}
+
+export function saveCourse(course) {
+  return function (dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => { 
+      throw(error);
+    });
+  }
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,11 +2,21 @@ import * as types from '../actions/actionTypes';
 import initialState from '../reducers/initialState';
 
 
-export default function courseReducer(state = initialState.courses, action) {
+export default function courseReducer(courseState = initialState.courses, action) {
   switch(action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
+    case types.CREATE_COURSE_SUCCESS:
+      return [
+        ...courseState,
+        Object.assign({}, action.course)
+      ];
+    case types.UPDATE_COURSE_SUCCESS:
+      return [
+        ...courseState.filter(course => course.id !== actions.course.id),
+        Object.assign({}, action.course)
+      ];
     default:
-      return state;
+      return courseState;
   }
 }
